d1f7f602ab0510d42ad6f90e1544003c43a22bf0
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-11-18 19:14:36 -0500

    Script now makes copies of red.png and blank.png with the generated file names, provided they don't already exist.

--- Textures/Things/Pawn/Animal/Apparel/generate_animal_pngs.py
+++ Textures/Things/Pawn/Animal/Apparel/generate_animal_pngs.py
@@ -1,26 +1,30 @@
 import os
+import shutil
 from dictionaries import *
 
 this_directory = os.path.dirname(os.path.abspath(__file__))
+red_png = '/'.join([this_directory, 'red.png'])
+blank_png = '/'.join([this_directory, 'blank.png'])
 
 for name in animals:
+    for blank in blank_files:
+        print(blank)
+        print(name)
+        filename = '/'.join([this_directory, blank.format(name)])
+        print(filename)
+        if os.path.isfile(filename):
+            print('File already exists.')
+        else:
+            print('File does not exist, creating now...')
+            shutil.copy(blank_png,filename)
+
     for red in red_files:
         print(red)
         print(name)
-
         filename = '/'.join([this_directory, red.format(name)])
         print(filename)
         if os.path.isfile(filename):
-            print('file exists')
+            print('File already exists.')
         else:
-            print('file does not exist')
-
-        input()
-        # filename = red.format(animal_name=name)
-        # file = os.commonpath(filename)
-        # full_path = file.path
-        # print(filename)
-        # print(full_path)
-    # for blank in blank_files:
-    #     filename = red.format(name)
-    #     print(filename)
+            print('File does not exist, creating now...')
+            shutil.copy(red_png,filename)
